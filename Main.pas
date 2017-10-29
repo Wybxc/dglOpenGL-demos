c8c212da436ab21235598c45be54ab76b3a831cb
Wybxc <1779724477@qq.com>
2017-10-29 10:20:16 +0800

    添加图形绘制

--- Main.pas
+++ Main.pas
@@ -17,6 +17,7 @@ type
     DC: HDC;
     RC: HGLRC;
     procedure Display(Sender: TObject; var Done: Boolean);
+    procedure glColorVertex(Color, Vertex: PGLfloat); inline;
   end;
 
 var
@@ -27,8 +28,59 @@ implementation
 {$R *.dfm}
 
 procedure TForm1.Display(Sender: TObject; var Done: Boolean);
+const
+  Red: array[1..3] of Single = (1, 0, 0);
+  Green: array[1..3] of Single = (0, 1, 0);
+  Blue: array[1..3] of Single = (0, 0, 1);
+  White: array[1..3] of Single = (1, 1, 1);
+  // 定义颜色常量
+  A: array[1..3] of Single = (0, 0, 0);
+  B: array[1..3] of Single = (2, 0, 0);
+  C: array[1..3] of Single = (1, 0, 1.732); // (1, 0, Sqrt(3))
+  P: array[1..3] of Single = (1, 1.732, 0.57735); // (1, Sqrt(3), 1 / Sqrt(3))
+  // 定义顶点常量
 begin
   glClear(GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT); // 清屏
+
+  // 绘图的操作在这里进行
+
+  glLoadIdentity;          // 还原坐标系
+  glTranslatef(0, 0, -5); // 坐标原点向屏幕内部移动5个单位长度
+
+  glBegin(GL_QUADS); // 绘制连续的矩形
+  begin              // 缩进辅助
+    glColor3f(1, 1, 0); // OpenGL.Pen.Color := RGB(1, 1, 0);
+    glVertex3f(0, 0, 0); // 第一个顶点(黄色), 位于坐标原点
+    glColor3f(1, 1, 1); // OpenGL.Pen.Color := RGB(1, 1, 1);
+    glVertex3f(1, 0, 0); // 第二、三、四个顶点(白色)
+    glVertex3f(1, 1, 0);
+    glVertex3f(0, 1, 0);
+  end;
+  glEnd;             // 结束绘图
+
+  glTranslatef(-3, 0, 0); // 坐标原点向左移动3个单位长度
+
+  glBegin(GL_TRIANGLES); // 绘制连续的三角形
+  begin
+    glColor3fv(@Red); // 使用指针, OpenGL.Pen.Color := ToColor(@Red);
+    glVertex3fv(@A);
+    glColorVertex(@Green, @B); // 定义简单方法 glColorVertex
+    glColorVertex(@Blue, @C);
+    // 底面完成
+    glColorVertex(@Red, @A);
+    glColorVertex(@Green, @B);
+    glColorVertex(@White, @P);
+    // 侧面完成 1/3
+    glColorVertex(@Green, @B);
+    glColorVertex(@Blue, @C);
+    glColorVertex(@White, @P);
+    // 侧面完成 2/3
+    glColorVertex(@Blue, @C);
+    glColorVertex(@Red, @A);
+    glColorVertex(@White, @P);
+    // 侧面完成
+  end;               // 正四面体
+  glEnd;             // 结束绘图
 
   SwapBuffers(DC); // 应用更改
 
@@ -38,7 +90,7 @@ end;
 procedure TForm1.FormCreate(Sender: TObject);
 begin
   DC := GetDC(Handle); // 获得 DC
-  if not InitOpenGL() then
+  if not InitOpenGL then
     raise Exception.Create('初始化 OpenGL 失败!'); // 初始化 OpenGL
   RC := CreateRenderingContext(DC, [opDoubleBuffered], 32, 24, 0, 0, 0, 0); // 获得 RC
   ActivateRenderingContext(DC, RC); // 绑定 DC, RC
@@ -78,6 +130,12 @@ begin
   (*********)
   glMatrixMode(GL_MODELVIEW); // 更改模型视图矩阵
   glLoadIdentity; // 设置当前矩阵为单位阵
+end;
+
+procedure TForm1.glColorVertex(Color, Vertex: PGLfloat);
+begin
+  glColor3fv(Color);
+  glVertex3fv(Vertex);
 end;
 
 end.
